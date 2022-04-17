510834d4c25eb5211948e0136f5c3d47bbfeab94
kabaniak <kabaniak@gmail.com>
2022-04-17 18:32:14 -0500

    styling

--- humoroo/src/App.js
+++ humoroo/src/App.js
@@ -10,10 +10,35 @@ function App() {
           <div className="row">
             <div className="col-sm"></div>
             <div className="col-sm">
-              <input type="text" className="form-control" placeholder="Word #1"></input>
+              <input type="text" className="form-control" placeholder="Noun #1"></input>
             </div>
             <div className="col-sm">
-              <input type="text" className="form-control" placeholder="Word #2"></input>
+              <input type="text" className="form-control" placeholder="Noun #2"></input>
+            </div>
+            <div className="col-sm"></div>
+          </div>
+        </div>
+
+        <div className="form-group">
+        <div className="row">
+            <div className="col-sm"></div>
+            <div className="col-sm">
+              <select class="custom-select my-2" id="selectNounType">
+                <option selected>Choose type...</option>
+                <option value="1">Person</option>
+                <option value="2">Place</option>
+                <option value="3">Event</option>
+                <option value="4">Thing</option>
+              </select>
+            </div>
+            <div className="col-sm">
+              <select class="custom-select my-2" id="selectNounType">
+                <option selected>Choose type...</option>
+                <option value="1">Person</option>
+                <option value="2">Place</option>
+                <option value="3">Event</option>
+                <option value="4">Thing</option>
+              </select>
             </div>
             <div className="col-sm"></div>
           </div>
